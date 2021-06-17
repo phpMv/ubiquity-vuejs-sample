792f77e800797de9e6dc9273ea35021d921dd357
jcheron <myaddressmail@gmail.com>
2021-06-17 03:34:40 +0200

    try axios

--- app/controllers/VueTestController.php
+++ app/controllers/VueTestController.php
@@ -1,9 +1,14 @@
 <?php
 namespace controllers;
+ use PHPMV\ajax\Http;
  use PHPMV\php\ubiquity\UVueManager;
  use PHPMV\VueJS;
  use PHPMV\VueManager;
+ use Ubiquity\attributes\items\router\Get;
+ use Ubiquity\attributes\items\router\Route;
  use Ubiquity\controllers\Controller;
+ use Ubiquity\controllers\Router;
+ use Ubiquity\utils\http\UResponse;
 
  /**
   * Controller VueTestController
@@ -15,6 +20,7 @@ class VueTestController extends Controller {
 		$this->vueManager=UVueManager::getInstance($this);
 	}
 
+	#[Route('_default',name:'vue.home')]
 	public function index(){
 		$vue=$this->vueManager->createVue('v-app',useVuetify: true);
 		$vue->addData('data','Hello world!');
@@ -22,5 +28,23 @@ class VueTestController extends Controller {
 		$vue->addData('select',[]);
 		$vue->addData('checkbox',true);
 		$this->vueManager->renderView("VueTestController/index.html");
+	}
+
+	#[Route('axios',name:'vue.axios')]
+	public function axiosTester(){
+		Http::init();
+		$vue=$this->vueManager->createVue('v-app',useVuetify: true);
+		$this->vueManager->setAxios(true);
+		$vue->addData('data','Hello world!');
+		$vue->addData('items',[]);
+		$vue->addData('select',[]);
+		$vue->addMethod('get','let self=this;'.Http::get(Router::path('vue.get'),null,'self.items=response.data;'));
+		$this->vueManager->renderDefaultView();
+	}
+
+	#[Get('get','vue.get')]
+	public function get(){
+		UResponse::asJSON();
+		echo \json_encode(['Programming','Design','Vue']);
 	}
 }
