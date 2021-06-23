9e6b18e50f2e33e72c7a82765f36bfa0926315b4
jcheron <myaddressmail@gmail.com>
2021-06-23 18:59:41 +0200

    add ubiquity-vuejs dependency

--- app/controllers/CrudUsers.php
+++ app/controllers/CrudUsers.php
@@ -1,48 +1,44 @@
 <?php
+
 namespace controllers;
- use models\User;
- use PHPMV\fw\ubiquity\UVueManager;
- use Ubiquity\contents\transformation\TransformersManager;
- use Ubiquity\orm\DAO;
- use Ubiquity\utils\models\UArrayModels;
 
- /**
-  * Controller CrudUsers
-  */
-class CrudUsers extends ControllerBase{
-	private ?UVueManager $vueManager;
+use models\User;
+use Ubiquity\controllers\vuejs\VuetifyJSController;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\models\UArrayModels;
 
-	public function initialize() {
-		$this->vueManager=UVueManager::getInstance($this);
-	}
-	protected function getfields(): array{
-		return ['id','firstname','lastname','suspended'];
+/**
+ * Controller CrudUsers
+ */
+class CrudUsers extends VuetifyJSController {
+
+	protected function getfields(): array {
+		return ['id', 'firstname', 'lastname', 'suspended'];
 	}
 
-	protected function getCaptions(): array{
-		return ['Id','Nom','Prénom','Suspendu'];
+	protected function getCaptions(): array {
+		return [];
 	}
 
-	protected function getHeaders(){
-		$res=[];
-		$captions=$this->getCaptions();
-		foreach ($this->getfields() as $index=>$field){
-			$res[]=['text'=>$captions[$index]??\ucfirst($field),'value'=>$field];
+	protected function getHeaders(): array {
+		$res = [];
+		$captions = $this->getCaptions();
+		foreach ($this->getfields() as $index => $field) {
+			$res[] = ['text' => $captions[$index] ?? \ucfirst($field), 'value' => $field];
 		}
 		return $res;
 	}
 
-	protected function getItemKey(){
+	protected function getItemKey(): string {
 		return 'id';
 	}
 
-	public function index(){
-		DAO::$useTransformers=true;
-		DAO::$transformerOp='toView';
-		$users=DAO::getAll(User::class);
-		$vue=$this->vueManager->createVue('v-app','app',true);
-		$vue->addData('users',UArrayModels::asArrayProperties($users,$this->getfields()));
-		$vue->addData('headers',$this->getHeaders());
-		$this->vueManager->renderView("CrudUsers/index.html",['item-key'=>$this->getItemKey()]);
+	public function index() {
+		DAO::$useTransformers = true;
+		DAO::$transformerOp = 'toView';
+		$users = DAO::getAll(User::class);
+		$this->vue->addData('users', UArrayModels::asArrayProperties($users, $this->getfields()));
+		$this->vue->addData('headers', $this->getHeaders());
+		$this->loadView('CrudUsers/index.html', ['item-key' => $this->getItemKey()]);
 	}
 }
